3ad43fcb93c8cb49d1a16a5fc618fe68e060c96e
mehedi9868 <mehedi9868@gmail.com>
2023-06-15 20:38:13 +0600

    demo banner image and text updated

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,42 +1,92 @@
 import { Fade } from 'react-awesome-reveal';
+import { Link } from 'react-router-dom';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
 const Banner = () => {
     return (
         <>
-            <Slide autoplay={false} duration={3000} pauseOnHover={false} pauseOnClick={false}>
+            <Slide autoplay={true} duration={5000} pauseOnHover={false} pauseOnClick={false}>
+                {/* slide 1 */}
                 <div className="relative w-screen">
-
-                    <Fade>
-                        <img src="https://img.freepik.com/free-photo/beautiful-scenery-rock-formations-by-sea-queens-bath-kauai-hawaii-sunset_181624-36857.jpg?w=1380&t=st=1686695113~exp=1686695713~hmac=dea082b5a42595cafb95a77cd6f18216e9791db57e065a6907cbae134ff23c5a" alt="Image 1" className="w-full brightness-75" />
-                    </Fade>
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
-
-
-
+                    <img src="https://i.ibb.co/tpvpwvK/img1-min.jpg" alt="Image 1" className="w-full brightness-50" />
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-200 text-center">
                         <Fade delay={1e3} cascade damping={1e-1}>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis odit molestias eligendi dolores adipisci pariatur facere maxime quod obcaecati praesentium?
-                            </p>
+                            <p className='text-5xl font-bold uppercase leading-normal space-y-5 text-gray-200'>
+                                Enroll now ! </p>
+                            <p className='text-3xl font-semibold'>For improving and advancing language proficiency <br />
+                                during the summer camp language learning program.</p>
+                            <Link to="/classes" className='btn btn-ghost bg-black bg-opacity-50 text-gray-200 mt-10'>ALL CLASSES</Link>
                         </Fade>
+                    </div>
+                </div>
 
-                    </div>
-                </div>
+                {/* slide 2 */}
                 <div className="relative w-screen">
-                    <img src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=1380&t=st=1686698236~exp=1686698836~hmac=2030997a60b8b50bd6c9d2af6b421264ebf847e67787221b6fa60f8d8f3993ac" alt="Image 2" className="w-full brightness-75" />
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
-                        <Fade>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis odit molestias eligendi dolores adipisci pariatur facere maxime quod obcaecati praesentium?
-                            </p>
+                    <img src="https://i.ibb.co/N2twBzc/img2-min.jpg" alt="Image 1" className="w-full brightness-50" />
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-200 text-center">
+                        <Fade delay={1e3} cascade damping={1e-1}>
+                            <p className='text-5xl font-bold uppercase leading-normal space-y-5 text-gray-200'>
+                                Enroll now ! </p>
+                            <p className='text-3xl font-semibold'>For improving and advancing language proficiency <br />
+                                during the summer camp language learning program.</p>
+                            <Link to="/classes" className='btn btn-ghost bg-black bg-opacity-50 text-gray-200 mt-10'>ALL CLASSES</Link>
                         </Fade>
                     </div>
                 </div>
+
+                {/* slide 3 */}
                 <div className="relative w-screen">
-                    <img src="https://img.freepik.com/free-photo/beautiful-scenery-rock-formations-by-sea-queens-bath-kauai-hawaii-sunset_181624-36857.jpg?w=1380&t=st=1686695113~exp=1686695713~hmac=dea082b5a42595cafb95a77cd6f18216e9791db57e065a6907cbae134ff23c5a" alt="Image 1" className="w-full brightness-75" />
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
-                        <Fade>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis odit molestias eligendi dolores adipisci pariatur facere maxime quod obcaecati praesentium?
-                            </p>
+                    <img src="https://i.ibb.co/t8q8y0s/img3-min.jpg" alt="Image 1" className="w-full brightness-50" />
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-200 text-center">
+                        <Fade delay={1e3} cascade damping={1e-1}>
+                            <p className='text-5xl font-bold uppercase leading-normal space-y-5 text-gray-200'>
+                                Enroll now ! </p>
+                            <p className='text-3xl font-semibold'>For improving and advancing language proficiency <br />
+                                during the summer camp language learning program.</p>
+                            <Link to="/classes" className='btn btn-ghost bg-black bg-opacity-50 text-gray-200 mt-10'>ALL CLASSES</Link>
+                        </Fade>
+                    </div>
+                </div>
+
+                {/* slide 4 */}
+                <div className="relative w-screen">
+                    <img src="https://i.ibb.co/t3p4XLD/img4-min.jpg" alt="Image 1" className="w-full brightness-50" />
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-200 text-center">
+                        <Fade delay={1e3} cascade damping={1e-1}>
+                            <p className='text-5xl font-bold uppercase leading-normal space-y-5 text-gray-200'>
+                                Enroll now ! </p>
+                            <p className='text-3xl font-semibold'>For improving and advancing language proficiency <br />
+                                during the summer camp language learning program.</p>
+                            <Link to="/classes" className='btn btn-ghost bg-black bg-opacity-50 text-gray-200 mt-10'>ALL CLASSES</Link>
+                        </Fade>
+                    </div>
+                </div>
+
+                {/* slide 5 */}
+                <div className="relative w-screen">
+                    <img src="https://i.ibb.co/BrgV1JH/img5-min.jpg" alt="Image 1" className="w-full brightness-50" />
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-200 text-center">
+                        <Fade delay={1e3} cascade damping={1e-1}>
+                            <p className='text-5xl font-bold uppercase leading-normal space-y-5 text-gray-200'>
+                                Enroll now ! </p>
+                            <p className='text-3xl font-semibold'>For improving and advancing language proficiency <br />
+                                during the summer camp language learning program.</p>
+                            <Link to="/classes" className='btn btn-ghost bg-black bg-opacity-50 text-gray-200 mt-10'>ALL CLASSES</Link>
+                        </Fade>
+                    </div>
+                </div>
+
+                {/* slide 6 */}
+                <div className="relative w-screen">
+                    <img src="https://i.ibb.co/yFq247s/img6-min.jpg" alt="Image 1" className="w-full brightness-50" />
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-200 text-center">
+                        <Fade delay={1e3} cascade damping={1e-1}>
+                            <p className='text-5xl font-bold uppercase leading-normal space-y-5 text-gray-200'>
+                                Enroll now ! </p>
+                            <p className='text-3xl font-semibold'>For improving and advancing language proficiency <br />
+                                during the summer camp language learning program.</p>
+                            <Link to="/classes" className='btn btn-ghost bg-black bg-opacity-50 text-gray-200 mt-10'>ALL CLASSES</Link>
                         </Fade>
                     </div>
                 </div>
