889be1ec987d48d4332203140cbeffca5e6191f8
mehedi9868 <mehedi9868@gmail.com>
2023-06-16 10:04:30 +0600

    popular classes will show top 6 class implemented

--- src/pages/Home/PopularClasses/PopularClasses.jsx
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import { AuthContext } from '../../../providers/AuthProvider';
+import { Fade } from 'react-awesome-reveal';
 
 const PopularClasses = () => {
     const [classData, setClassData] = useState([])
@@ -46,21 +47,24 @@ const PopularClasses = () => {
     console.log(classData)
 
     return (
-        <div className='md:p-10 my-10 flex flex-col'>
-            <p className='text-3xl font-bold mb-10 text-center'>Popular Classes</p>
-            <div className='grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 mx-auto gap-5'>
-                {classData.map((classes) =>
-                    <div key={classes._id} className={`card w-80 group glass ${classes.seats === 0 && 'bg-red-600'}`}>
-                        <figure><img className='w-80 h-80 group-hover:scale-110' src={classes.image} alt="car!" /></figure>
-                        <div className="card-body">
-                            <p className='font-semibold'>Class name: <span className='font-normal'>{classes.className}</span></p>
-                            <p className='font-semibold'>Instructor Name: <span className='font-normal'>{classes.instructorName}</span></p>
-                            <p className='font-semibold'>Available Seats: <span className='font-normal'>{classes.seats}</span></p>
-                            <p className='font-semibold'>Enrolled Students: <span className='font-normal'>{classes?.enrolledStudents}</span></p>
-                            <p className='font-semibold'>Price: <span className='font-normal'>${classes.price}</span></p>
-                            <button disabled={classes.seats === 0 ? 'disabled' : currentUser.role === 'admin' || currentUser.role === 'instructor' ? 'disabled' : ''} onClick={() => handleSelect(classes)} className="btn btn-primary">Select</button>
+        <div className='flex flex-col'>
+            <p className='text-3xl font-bold my-20 text-center'>Popular Classes</p>
+            <div className='grid grid-cols-1 md:grid-cols-3 mx-auto gap-5'>
+                {classData.slice(0, 6).map((classes) =>
+                    <Fade key={classes._id}>
+                        <div className={`card w-full h-full rounded-md group glass ${classes.seats === 0 && 'bg-red-600'}`}>
+                            <figure><img className='w-full h-80 group-hover:scale-110' src={classes.image} alt="car!" /></figure>
+                            <div className="card-body">
+                                <p className='font-semibold'>Class name: <span className='font-normal'>{classes.className}</span></p>
+                                <p className='font-semibold'>Instructor Name: <span className='font-normal'>{classes.instructorName}</span></p>
+                                <p className='font-semibold'>Available Seats: <span className='font-normal'>{classes.seats}</span></p>
+                                <p className='font-semibold'>Enrolled Students: <span className='font-normal'>{classes?.enrolledStudents}</span></p>
+                                <p className='font-semibold'>Price: <span className='font-normal'>${classes.price}</span></p>
+                                <button disabled={classes.seats === 0 ? 'disabled' : currentUser.role === 'admin' || currentUser.role === 'instructor' ? 'disabled' : ''} onClick={() => handleSelect(classes)} className="btn btn-primary">Select</button>
+                            </div>
                         </div>
-                    </div>)}
+                    </Fade>
+                )}
             </div>
         </div>
     );
